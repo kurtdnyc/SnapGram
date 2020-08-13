ed88333e63592328d7a9b1567e9c9df0b2c4694c
mnauman319 <mnauman319@gmail.com>
2020-08-13 18:39:01 -0500

    edited controllers to send 201 for create, and added getAllPhotos service

--- src/main/java/dev/snapgram/controllers/PhotoController.java
+++ src/main/java/dev/snapgram/controllers/PhotoController.java
@@ -1,19 +1,27 @@
 package dev.snapgram.controllers;
 
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import dev.snapgram.entities.Photo;
+import dev.snapgram.entities.Tag;
 import dev.snapgram.entities.User;
 import dev.snapgram.services.PhotoService;
 import dev.snapgram.services.PhotoServiceImpl;
+import dev.snapgram.services.TagService;
 import dev.snapgram.services.UserService;
 import dev.snapgram.services.UserServiceImpl;
 
@@ -25,10 +33,12 @@ public class PhotoController {
 	PhotoService pServ;
 	@Autowired
 	UserService uServ;
-
+	@Autowired
+	TagService tServ;
 	
 	@RequestMapping(value = "/users/{uid}/photos", method = RequestMethod.POST)
 	@ResponseBody
+	@ResponseStatus(HttpStatus.CREATED)
 	public Photo createPhoto(@RequestBody Photo photo, @PathVariable int uid) {
 		
 		// first get userbyuserid
@@ -47,7 +57,19 @@ public class PhotoController {
 		return this.pServ.getPhotoById(pid);
 	}
 	
-	
+	@RequestMapping(value = "/users/{uid}/photos",method = RequestMethod.GET)
+	@ResponseBody
+	public Set<Photo> getPhotos(@PathVariable int uid,@RequestParam(required = false) String hashtag){
+		if(hashtag != null) {
+			Set<Tag> tags = this.tServ.getTagsByName(hashtag);
+			Set<Photo> photos = new HashSet<Photo>();
+			for(Tag tag : tags) {
+				photos.add(tag.getPhoto());
+			}
+			return photos;
+		}
+		return this.uServ.getUserById(uid).getPhotos();
+	}
 	
 	@RequestMapping(value = "/users/{uid}/photos", method = RequestMethod.PUT)
 	@ResponseBody
--- src/main/java/dev/snapgram/controllers/TagController.java
+++ src/main/java/dev/snapgram/controllers/TagController.java
@@ -1,20 +1,24 @@
 package dev.snapgram.controllers;
 
+import java.util.HashSet;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import dev.snapgram.entities.Photo;
 import dev.snapgram.entities.Tag;
 import dev.snapgram.services.TagService;
-import dev.snapgram.services.TagServiceImpl;
+
 
 @Component
 @Controller
@@ -22,17 +26,22 @@ public class TagController {
 
 	@Autowired
 	TagService tServ;
+
 	
-	@RequestMapping(value = "/tags", method = RequestMethod.POST)
+	@RequestMapping(value = "users/{uid}/photos/{pid}/tags", method = RequestMethod.POST)
 	@ResponseBody
+	@ResponseStatus(HttpStatus.CREATED)
 	public Tag createTag(@RequestBody Tag tag) {
+		
 		return this.tServ.createTag(tag);
 	}
 	
 	
-	@RequestMapping(value = "/tags/{tid}", method = RequestMethod.GET)
+	@RequestMapping(value = "users/{uid}/photos/{pid}/tags/{tid}", method = RequestMethod.GET)
 	@ResponseBody
 	public Photo getPhotosByTag(@PathVariable int tid) {
+		
 		return this.tServ.getTagById(tid).getPhoto();
 	}
+	
 }
