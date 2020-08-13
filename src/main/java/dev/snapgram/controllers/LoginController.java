51aef8f9c9c3ced703a502b5d538c36210f1bcae
Kurt Dawiec <kurtmdawiec@gmail.com>
2020-08-13 16:16:08 -0400

    .

--- src/main/java/dev/snapgram/controllers/LoginController.java
+++ src/main/java/dev/snapgram/controllers/LoginController.java
@@ -1,17 +1,26 @@
 package dev.snapgram.controllers;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import dev.snapgram.dtos.LoginDTO;
 import dev.snapgram.entities.User;
 import dev.snapgram.exceptions.InvalidLoginException;
 import dev.snapgram.services.UserService;
 
+@Component
+@RestController
 public class LoginController {
 
 	@Autowired
@@ -19,25 +28,17 @@ public class LoginController {
 	
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
 	@ResponseBody
-	public User loginUser(@RequestBody LoginDTO dto) {
-		try {
-			User user = this.uServ.getUserByLogin(dto.getUsername(), dto.getPassword());
-			// create session attribute
-			// redirect to home page
-			return null;
-		} catch (InvalidLoginException e) {
-			e.toString();
-			//try again
-			return null;
-		}
-		
+	public User loginUser(@RequestBody LoginDTO dto, HttpServletResponse httpServletResponse) throws InvalidLoginException, IOException {
+		User user = this.uServ.getUserByLogin(dto.getUsername(), dto.getPassword());
+		httpServletResponse.sendRedirect("https://twitter.com/");
+
+		return user;
 	}
 	
-	@RequestMapping(value = "/login", method = RequestMethod.GET)
-	@ResponseBody
-	public User getCurrentSession() {
-		// get user from session attribute
-		return null;
-		
-	}
+//	@RequestMapping(value = "/login", method = RequestMethod.GET)
+//	@ResponseBody
+//	public User getCurrentSession(HttpSession session){
+//		User user = (User) session.getAttribute("loggedInAs");
+//		return user;
+//	}
 }
