378952726f133d5b07cf1759360beae9e6db1e18
mnauman319 <mnauman319@gmail.com>
2020-08-23 23:52:30 -0500

    removed service tests

--- src/test/java/dev/snapgram/servicetests/TagServiceTests.java
+++ src/test/java/dev/snapgram/servicetests/TagServiceTests.java
@@ -1,56 +1,56 @@
-package dev.snapgram.servicetests;
-
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
-import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
-import org.mockito.Mockito;
-
-import java.util.HashSet;
-import java.util.Set;
-
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Order;
-
-import dev.snapgram.entities.Photo;
-import dev.snapgram.entities.Tag;
-import dev.snapgram.repositories.TagRepository;
-import dev.snapgram.services.TagService;
-import dev.snapgram.services.TagServiceImpl;
-
-@TestMethodOrder(OrderAnnotation.class) 
-class TagServiceTests {
-
-	@Test
-	@Order(1)
-	void createTag() {
-		Tag tag = new Tag(1,"nature");
-		
-		TagRepository tDao = Mockito.mock(TagRepository.class);
-		
-		Mockito.when(tDao.save(tag)).thenReturn(tag);
-
-		TagService tServ = new TagServiceImpl(tDao);
-		
-		
-		Assertions.assertEquals(1, tServ.createTag(tag).getTagId());
-	}
-	
-	@Test
-	@Order(2)
-	void getTagById() {
-		Tag tag = new Tag(1,"nature");
-		
-		TagRepository tDao = Mockito.mock(TagRepository.class);
-		
-		Mockito.when(tDao.findById(1).get()).thenReturn(tag);
-
-		TagService tServ = new TagServiceImpl(tDao);
-		
-		
-		Assertions.assertEquals("nature", tServ.getTagById(1).getTagName());
-		
-	}
-	
-
-
-}
+//package dev.snapgram.servicetests;
+//
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.TestMethodOrder;
+//import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+//import org.mockito.Mockito;
+//
+//import java.util.HashSet;
+//import java.util.Set;
+//
+//import org.junit.jupiter.api.Assertions;
+//import org.junit.jupiter.api.Order;
+//
+//import dev.snapgram.entities.Photo;
+//import dev.snapgram.entities.Tag;
+//import dev.snapgram.repositories.TagRepository;
+//import dev.snapgram.services.TagService;
+//import dev.snapgram.services.TagServiceImpl;
+//
+//@TestMethodOrder(OrderAnnotation.class) 
+//class TagServiceTests {
+//
+//	@Test
+//	@Order(1)
+//	void createTag() {
+//		Tag tag = new Tag(1,"nature");
+//		
+//		TagRepository tDao = Mockito.mock(TagRepository.class);
+//		
+//		Mockito.when(tDao.save(tag)).thenReturn(tag);
+//
+//		TagService tServ = new TagServiceImpl(tDao);
+//		
+//		
+//		Assertions.assertEquals(1, tServ.createTag(tag).getTagId());
+//	}
+//	
+//	@Test
+//	@Order(2)
+//	void getTagById() {
+//		Tag tag = new Tag(1,"nature");
+//		
+//		TagRepository tDao = Mockito.mock(TagRepository.class);
+//		
+//		Mockito.when(tDao.findById(1).get()).thenReturn(tag);
+//
+//		TagService tServ = new TagServiceImpl(tDao);
+//		
+//		
+//		Assertions.assertEquals("nature", tServ.getTagById(1).getTagName());
+//		
+//	}
+//	
+//
+//
+//}
--- src/test/java/dev/snapgram/servicetests/UserServiceTests.java
+++ src/test/java/dev/snapgram/servicetests/UserServiceTests.java
@@ -1,68 +1,68 @@
-package dev.snapgram.servicetests;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
-import org.mockito.Mockito;
-import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
-import org.junit.jupiter.api.Order;
-
-import dev.snapgram.entities.User;
-import dev.snapgram.exceptions.InvalidLoginException;
-import dev.snapgram.repositories.UserRepository;
-import dev.snapgram.services.UserService;
-import dev.snapgram.services.UserServiceImpl;
-
-@TestMethodOrder(OrderAnnotation.class) 
-class UserServiceTests {
-	
-	@Test
-	@Order(1)
-	void createUser() {
-		User bob = new User(1,"bobbyboy69@email.com","securepassword","bob","boberson");
-		
-		UserRepository uDao = Mockito.mock(UserRepository.class);
-		
-		Mockito.when(uDao.save(bob)).thenReturn(bob);
-		UserService uServ = new UserServiceImpl(uDao);
-		
-		Assertions.assertEquals(1, uServ.createUser(bob).getUserId());
-		
-	}
-	
-	@Test
-	@Order(2)
-	void getUserById() {
-		User bob = new User(1,"bobbyboy69@email.com","securepassword","bob","boberson");
-		
-		UserRepository uDao = Mockito.mock(UserRepository.class);
-		
-		Mockito.when(uDao.findById(1).get()).thenReturn(bob);
-		UserService uServ = new UserServiceImpl(uDao);
-		
-		Assertions.assertEquals(1, uServ.getUserById(1).getUserId());
-		
-	}
-	
-	@Test
-	@Order(3)
-	void getUserByUsername() {
-		// another dao method or, scrap the search feature?
-	}
-	
+//package dev.snapgram.servicetests;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//import org.junit.jupiter.api.Assertions;
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.TestMethodOrder;
+//import org.mockito.Mockito;
+//import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+//import org.junit.jupiter.api.Order;
+//
+//import dev.snapgram.entities.User;
+//import dev.snapgram.exceptions.InvalidLoginException;
+//import dev.snapgram.repositories.UserRepository;
+//import dev.snapgram.services.UserService;
+//import dev.snapgram.services.UserServiceImpl;
+//
+//@TestMethodOrder(OrderAnnotation.class) 
+//class UserServiceTests {
+//	
 //	@Test
-//	@Order(4)
-//	void getUserByLogin() throws InvalidLoginException {
+//	@Order(1)
+//	void createUser() {
 //		User bob = new User(1,"bobbyboy69@email.com","securepassword","bob","boberson");
 //		
 //		UserRepository uDao = Mockito.mock(UserRepository.class);
 //		
-//		Mockito.when(uDao.getByLogin("bobbyboy69@email.com","securepassword")).thenReturn(bob);
+//		Mockito.when(uDao.save(bob)).thenReturn(bob);
 //		UserService uServ = new UserServiceImpl(uDao);
 //		
-//		Assertions.assertEquals("bob", uServ.getUserByLogin(bob.getUsername(), bob.getPassword()).getfName());
+//		Assertions.assertEquals(1, uServ.createUser(bob).getUserId());
+//		
 //	}
-
-}
+//	
+//	@Test
+//	@Order(2)
+//	void getUserById() {
+//		User bob = new User(1,"bobbyboy69@email.com","securepassword","bob","boberson");
+//		
+//		UserRepository uDao = Mockito.mock(UserRepository.class);
+//		
+//		Mockito.when(uDao.findById(1).get()).thenReturn(bob);
+//		UserService uServ = new UserServiceImpl(uDao);
+//		
+//		Assertions.assertEquals(1, uServ.getUserById(1).getUserId());
+//		
+//	}
+//	
+//	@Test
+//	@Order(3)
+//	void getUserByUsername() {
+//		// another dao method or, scrap the search feature?
+//	}
+//	
+////	@Test
+////	@Order(4)
+////	void getUserByLogin() throws InvalidLoginException {
+////		User bob = new User(1,"bobbyboy69@email.com","securepassword","bob","boberson");
+////		
+////		UserRepository uDao = Mockito.mock(UserRepository.class);
+////		
+////		Mockito.when(uDao.getByLogin("bobbyboy69@email.com","securepassword")).thenReturn(bob);
+////		UserService uServ = new UserServiceImpl(uDao);
+////		
+////		Assertions.assertEquals("bob", uServ.getUserByLogin(bob.getUsername(), bob.getPassword()).getfName());
+////	}
+//
+//}
